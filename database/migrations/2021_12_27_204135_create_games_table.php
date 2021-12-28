187a96e08062ee36f97d59826fcb9721f9ffb271
Nicolas Farfán Cheneaux <nicolas.farfan.ch@usach.cl>
2021-12-28 11:57:43 -0300

    [Nícolas Farfán] Se corrigen con llaves foráneas

--- database/migrations/2021_12_27_204135_create_games_table.php
+++ database/migrations/2021_12_27_204135_create_games_table.php
@@ -6,43 +6,41 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateGamesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('games', function (Blueprint $table) {
-            $table->id();
-            $table->string('name',50);
-            $table->string('description',1000);
-            $table->float('price', 4,2);
-            $table->integer('sales');
-            $table->string('classification',50);
-            $table->string('valoration', 50);
-            $table->float('storage', 4, 2);
-            $table->unsignedBigInteger('country_id')->nullable();
-            $table->foreign('country_id')->references('id')->on('countries');
-            $table->unsignedBigInteger('gender_id')->nullable();
-            $table->foreign('gender_id')->references('id')->on('genders');    
-            $table->unsignedBigInteger('age_restriction_id')->nullable();
-            $table->foreign('age_restriction_id')->references('id')->on('age_restrictions');  
-            $table->unsignedBigInteger('url_id')->nullable();
-            $table->foreign('url_id')->references('id')->on('urls');   
-            $table->unsignedBigInteger('demo_id')->nullable();
-            $table->foreign('demo_id')->references('id')->on('demos');      
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('games', function (Blueprint $table) {
+      $table->id();
+      $table->string('name', 50);
+      $table->string('description', 1000);
+      $table->float('price', 4, 2);
+      $table->integer('sales');
+      $table->string('classification', 50);
+      $table->string('valoration', 50);
+      $table->float('storage', 4, 2);
+      $table->unsignedBigInteger('gender_id')->nullable();
+      $table->foreign('gender_id')->references('id')->on('genders');
+      $table->unsignedBigInteger('age_restriction_id')->nullable();
+      $table->foreign('age_restriction_id')->references('id')->on('age_restrictions');
+      $table->unsignedBigInteger('url_id')->nullable();
+      $table->foreign('url_id')->references('id')->on('urls');
+      $table->unsignedBigInteger('demo_id')->nullable();
+      $table->foreign('demo_id')->references('id')->on('demos');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('games');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('games');
+  }
 }
---
+++ database/migrations/2021_12_28_145110_create_likes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikesTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('likes', function (Blueprint $table) {
+      $table->id();
+      $table->boolean('like');
+      $table->unsignedBigInteger('user_id')->nullable();
+      $table->foreign('user_id')->references('id')->on('users');
+      $table->unsignedBigInteger('game_id')->nullable();
+      $table->foreign('game_id')->references('id')->on('games');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('likes');
+  }
+}
