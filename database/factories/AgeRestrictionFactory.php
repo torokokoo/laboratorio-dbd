a6cfe894188ae461d2e35ba4a5e53cddbba8a740
nic0q <nicolas.farfan.ch@usach.cl>
2021-12-30 23:30:43 -0300

    [Nícolas Farfán] Age_Restriction faker

--- database/factories/AgeRestrictionFactory.php
+++ database/factories/AgeRestrictionFactory.php
@@ -6,15 +6,15 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class AgeRestrictionFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array
+   */
+  public function definition()
+  {
+    return [
+      'age_restriction' => $this->faker->numberBetween($min = 0, $max = 90)
+    ];
+  }
 }
